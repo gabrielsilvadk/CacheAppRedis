9ff0737edbe7e2fa2548e67111a20787fccc0e77
gabrielsilvadk <gabriel.silvadk@gmail.com>
2025-02-02 22:11:10 -0300

    uploading the entire project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using StackExchange.Redis;
+
+string connectionString = "";
+
+using (var cache = ConnectionMultiplexer.Connect(connectionString))
+{
+    IDatabase db = cache.GetDatabase();
+
+    var result = await db.ExecuteAsync("PING");
+    Console.WriteLine(result);
+
+    bool setValue = await db.StringSetAsync("name", "DIO gsilva");
+    Console.WriteLine(setValue);
+    
+    string getValue = await db.StringGetAsync("name");
+    Console.WriteLine(getValue);
+}
